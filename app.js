30812da0307871d77c170afd9556b3b1bad24838
DoffoGitAdmin <lalit.nagar@octalsoftware.com>
2025-02-11 11:50:24 +0530

    updeted yml

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 2200;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('This is Devops Blugreen!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ scripts/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /home/ubuntu/test
+cd /home/ubuntu/test
+chmod +x /home/ubuntu/test/scripts/after_install.sh
+
+npm install
